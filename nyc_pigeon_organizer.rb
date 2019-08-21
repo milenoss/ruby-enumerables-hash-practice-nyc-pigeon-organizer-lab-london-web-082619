bc4e065cdb758cd07f64be40c625bfbe5942a93d
Milen Khanal <milenkhanal@gmail.com>
2019-08-21 17:28:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -40,7 +40,7 @@ def nyc_pigeon_organizer (data)
       
       array.each do |name|
         final[name] = {:color => [], :gender => [], :lives => []}
-        binding.pry
+        
       end 
     end 
   end 
@@ -48,6 +48,7 @@ def nyc_pigeon_organizer (data)
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
       x.each do |item|
+        binding.pry
         if bird_name === item
           final[item][:color] << bird_color.to_s
         end 
